47a21d670f459734fcc0e9eff8aebf4707acf146
Sophie Peneva <39370712+sophiepeneva@users.noreply.github.com>
2018-05-22 21:58:38 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+# First-Java-project
+random java classes
+
+public class Main {
+    public static void main(String[] args) {
+
+        User[] users = new  User[3];
+        users[0] = new  User("Sophie","piche98");
+
+        System.out.println(users[0].getPassword());
+    }
+}
